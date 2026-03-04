59285f548bc2894455b035fdfb741d9e74dba9a2
vutienmanh123plhd-wq <vutienmanh123plhd@gmail.com>
2026-03-04 22:01:13 +0700

    Delete Web/Online Test Web directory

--- Web/Online Test Web/src/app/components/layouts/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router';
-
-export function AuthLayout() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4">
-      <Outlet />
-    </div>
-  );
-}
